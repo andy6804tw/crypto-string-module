3f650b32abbf9540e923bcc9f8eea4690632c0c6
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-02-28 11:02:41 +0800

    done api

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const http = require('http');
+
 module.exports = {
   RandomChar: (num) => {
     const string = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -6,5 +8,40 @@ module.exports = {
       str += string.charAt(Math.floor(Math.random() * string.length));
     }
     return str;
+  },
+  latest: (str) => {
+    return new Promise((resolve, reject) => {
+      if (str) {
+        apiRequest(str, resolve, reject);
+      }
+    })
   }
+}
+
+
+// function
+const apiRequest = (str, resolve, reject) => {
+  const url = {
+    host: 'api.fixer.io',
+    path: '/' + str,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  http.get(url, (response) => {
+    let body = [];
+    response.on('data', (chunk) => {
+      //console.log(chunk);
+      body.push(chunk);
+    });
+    response.on('end', () => {
+      if (response.statusCode == 200) {
+        const result = Buffer.concat(body).toString();
+        const newResult = JSON.parse(result);
+        resolve(newResult);
+      } else {
+        reject('error');
+      }
+    })
+  })
 }
